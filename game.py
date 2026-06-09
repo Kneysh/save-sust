400980e79ffe9b39ade173c36b410fb86cfd578c
Md. Nayeemujjaman Khondoker Neyshan <neyshankhondoker@gmail.com>
2026-06-09 21:56:32 +0600

    feat: Create and animate Player

--- game.py
+++ game.py
@@ -11,7 +11,44 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = load_image("sust.png")
+        self.rect = self.image.get_rect(midbottom=(400,590))
+        self.imageWidth = 100
+        self.imageHeight = 100
 
+        self.x_change = 0
+        self.y_change = 0
+        self.playerSpeed = 5
+    
+    def instructions(self):
+        keys = pygame.key.get_pressed()
+        self.x_change = 0
+        self.y_change = 0
+
+        if keys[pygame.K_RIGHT] and self.rect.right <= 800:
+            self.x_change = (self.playerSpeed)
+        if keys[pygame.K_LEFT] and self.rect.left >= 0:
+            self.x_change = -(self.playerSpeed)
+        if keys[pygame.K_DOWN] and self.rect.bottom <= 600:
+            self.y_change = (self.playerSpeed)
+        if keys[pygame.K_UP] and self.rect.top >= 0:
+            self.y_change = -(self.playerSpeed)
+    
+    def move(self):
+        self.rect.left += self.x_change
+        self.rect.top += self.y_change
+
+        if self.rect.bottom >= 600:
+            self.rect.bottom = 600
+        if self.rect.right >= 800:
+            self.rect.right = 800
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.left <= 0:
+            self.rect.left = 0
+    
+    def update(self):
+        self.instructions()
+        self.move()
 
 
 # obstacles class
@@ -40,8 +77,17 @@ class Game():
         # background image
         self.background = load_image("background.png")
         # self.backgroundRect = self.background.get_rect()
-        self.backgroundMove = 5
+        self.backgroundMove = 3
         self.backgroundY = self.displayHeight - 1536
+
+
+        # groups
+        self.player = pygame.sprite.GroupSingle()
+        self.player.add(Player())
+
+        self.x_change = 0
+        self.y_change = 0
+        self.playerSpeed = 5
 
         self.gameOver = False
 
@@ -62,6 +108,9 @@ class Game():
 
 
 
+            # player
+            self.player.draw(self.screen)
+            self.player.update()
 
             pygame.display.update()
             self.clock.tick(60)
